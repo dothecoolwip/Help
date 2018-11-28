daa3a715f4c8bc92680df094c389c7cc766f8116
Garrett <dothecoolwip@gmail.com>
2018-11-28 11:35:37 +0700

    help

---
+++ layouts/partials/nav.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="body">
+    <nav>
+      <h1 class="brand" id="href"><nuxt-link to ="/">Friendly angkor tuk tuk</nuxt-link></h1>
+      <ul>
+        <li><nuxt-link to ="/">Home</nuxt-link></li>
+        <li><nuxt-link to ="About">About</nuxt-link></li>
+        <li><nuxt-link to ="Prices">Prices</nuxt-link></li>
+        <li><nuxt-link to ="Gallery">Gallery</nuxt-link></li>
+      </ul>
+    </nav>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+section{
+  margin: 0;
+  padding: 0;
+}
+nav{
+  width: 100%;
+  background: black;
+  padding: 0 50px;
+  box-sizing: border-box;
+}
+nav h1{
+  margin: 0;
+  padding: 0;
+  float: left;
+}
+
+nav h1 a{
+  font-size: 20px;
+  color: #fff;
+  text-decoration: none;
+}
+nav ul{
+  margin: 0;
+  padding: 0;
+  float: right;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+    <h1>Angkor Family Tours</h1>
+    <p class="subheading">weve got the best nuxt</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore est tempora inventore modi adipisci magnam molestias laborum error nobis atque. Quaerat officia corporis officiis voluptatem totam, vel amet cupiditate. Dolores.</p>
+  </section>
+</template>
+
+<script>
+
+
+export default {
+transition: "bounce",
+    head(){
+        return{
+            titleTemplate: "%s - Home"
+        };
+    }
+}
+
+</script>
+
+<style>
+</style>
